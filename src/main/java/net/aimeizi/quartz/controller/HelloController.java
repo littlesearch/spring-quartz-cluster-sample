4a5ff3d7d68460271ef4bd3a05dd3f34883d7135
Tan Liwei <236328172@qq.com>
2018-11-26 17:49:09 +0800

    minor

--- src/main/java/net/aimeizi/quartz/controller/HelloController.java
+++ src/main/java/net/aimeizi/quartz/controller/HelloController.java
@@ -4,8 +4,6 @@ package net.aimeizi.quartz.controller;
 import com.kowalski.demo.IPAddressKowalski;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
